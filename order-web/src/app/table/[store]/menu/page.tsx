a321b56c4b0e1468b0582d11d7f41189bc647473
김범기 <112@16612298.com>
2026-05-12 15:46:19 +0900

    주문 메뉴리스트 페이지 수정

--- order-web/src/app/table/[store]/menu/page.tsx
+++ order-web/src/app/table/[store]/menu/page.tsx
@@ -1,13 +1,16 @@
 import PageMenuList from "@/components/pages/PageMenuList";
+import { getStoreData } from "@/app/api/store";
 
-export default function Menupage({
+export default async function Menupage({
   params,
 }: {
   params: Promise<{ store: string }>;
 }) {
+  const storeData = await getStoreData();
+
   return (
     <div>
-      <PageMenuList params={params} />
+      <PageMenuList params={params} initialData={storeData} />
     </div>
   );
 }
